b714fb86e49817e6296eb74b7222bac472537a9d
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-09 22:46:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,7 +16,7 @@ joined_strings = ""
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  while element_index < src.count do
+  while element_index < src[row_index].count do
     if src[row_index][element_index].class == string
       joined_strings += src[row_index][element_index] + ''
     end
